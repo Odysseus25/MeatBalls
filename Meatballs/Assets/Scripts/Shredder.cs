c87ab536f0c89e6b7b1d011bbd5a6dbee63347f5
Ulises González Zúñiga <ulisesgz@outlook.com>
2016-04-12 11:40:16 -0600

    Shredded added

---
+++ Meatballs/Assets/Scripts/Shredder.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shredder : MonoBehaviour {
+
+    void OnTriggerEnter(Collider col) {
+        Ingredients ingredient = col.GetComponent<Ingredients>();
+        if (ingredient) {
+            Destroy(ingredient.gameObject);
+        }
+    }
+
+}
